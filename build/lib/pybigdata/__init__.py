a93033f41b0f7a6e22806bead26ee3e120c853c0
Alireza Sheikhiy <152708740+AlirezaSheikhiy@users.noreply.github.com>
2024-02-04 03:45:58 +0330

    Delete some folders

--- build/lib/pybigdata/__init__.py
+++
@@ -1,6 +0,0 @@
-from pybigdata.searching import *
-from pybigdata.sorting import *
-from pybigdata.file_reader import *
-from pybigdata.linear_algebra import *
-from pybigdata.optimization import *
-from pybigdata.probabilities import *
